13eea05521dd46483674f7f70ecddd7ce7a8606a
sabinaaj <sabina.aj@seznam.cz>
2024-07-16 16:17:12 +0200

    Plant list and choose schooltype

--- areas/views.py
+++ areas/views.py
@@ -8,3 +8,33 @@ class AreaListView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         return context
+
+class AreaCreateView(TemplateView):
+    template_name = "areas_create.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        return context
+
+class AreaUpdateView(TemplateView):
+    template_name = "areas_update.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        return context
+
+
+class AreaDeleteView:
+    pass
+
+class PlantListView(TemplateView):
+    template_name = "plants_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
