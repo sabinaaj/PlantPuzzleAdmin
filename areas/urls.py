a69f0cbae8486761bf6da52813b0f36e03a2765b
Sabina <sabina.aj@seznam.cz>
2024-07-28 21:06:51 +0200

    areas BE

--- areas/urls.py
+++ areas/urls.py
@@ -5,10 +5,10 @@ from .views import AreaListView, AreaCreateView, AreaUpdateView, AreaDeleteView,
 app_name = "areas"
 
 urlpatterns = [
-    path("list", AreaListView.as_view(), name="area_list"),
-    path("create", AreaCreateView.as_view(), name="area_create"),
-    path("update/<int:pk>", AreaUpdateView.as_view(), name="area_update"),
-    #path("delete/<int:pk>", AreaDeleteView.as_view(), name="area_delete"),
+    path('list/', AreaListView.as_view(), name='area_list'),
+    path('create/', AreaCreateView.as_view(), name='area_create'),
+    path('<int:pk>/update/', AreaUpdateView.as_view(), name='area_update'),
+    path('<int:pk>/delete/', AreaDeleteView.as_view(), name='area_delete'),
     path("plants/list", PlantListView.as_view(), name="plants_list"),
 ]
 
--- areas/views.py
+++ areas/views.py
@@ -1,35 +1,37 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, UpdateView, DeleteView, ListView
+from django.urls import reverse, reverse_lazy
+from django.shortcuts import get_object_or_404
 
-class AreaListView(TemplateView):
+from areas.forms import AreaForm
+from areas.models import Area
+
+
+class AreaListView(ListView):
+    model = Area
     template_name = "areas_list.html"
-
-    def get_context_data(self, **kwargs):
-
-        context = super().get_context_data(**kwargs)
-
-        return context
-
-class AreaCreateView(TemplateView):
-    template_name = "areas_create.html"
-
-    def get_context_data(self, **kwargs):
-
-        context = super().get_context_data(**kwargs)
-
-        return context
-
-class AreaUpdateView(TemplateView):
-    template_name = "areas_update.html"
-
-    def get_context_data(self, **kwargs):
-
-        context = super().get_context_data(**kwargs)
-
-        return context
+    context_object_name = "areas"
 
 
-class AreaDeleteView:
-    pass
+class AreaCreateView(CreateView):
+    template_name = "areas_form.html"
+    form_class = AreaForm
+    model = Area
+
+    def get_success_url(self):
+        return reverse("areas:area_list")
+
+
+class AreaUpdateView(UpdateView):
+    template_name = "areas_form.html"
+    form_class = AreaForm
+    model = Area
+
+    def get_success_url(self):
+        return reverse("areas:area_list")
+
+class AreaDeleteView(DeleteView):
+    model = Area
+    success_url = reverse_lazy("areas:area_list")
 
 class PlantListView(TemplateView):
     template_name = "plants_list.html"
